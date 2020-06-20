f44d13571609eea771883e7588eefb7d15b1dc70
Robert Dolca <robert.dolca@gmail.com>
2020-06-20 14:49:33 -0700

    Add sync params

--- commands/sync/sync.go
+++ commands/sync/sync.go
@@ -10,6 +10,10 @@ import (
 
 type authListCmd struct {
 	sync *calendar.Manager
+	srcAccount string
+	srcCalendar string
+	dstAccount string
+	dstCalendar string
 }
 
 func New(sync *calendar.Manager) subcommands.Command  {
@@ -27,13 +31,18 @@ func (*authListCmd) Synopsis() string {
 }
 
 func (*authListCmd) Usage() string {
-	return ``
+	return "calendar sync\n"
 }
 
-func (p *authListCmd) SetFlags(*flag.FlagSet) {}
+func (p *authListCmd) SetFlags(f *flag.FlagSet) {
+	f.StringVar(&p.srcAccount, "src-account", "", "Source account email address")
+	f.StringVar(&p.srcCalendar, "src-calendar", "", "Source calendar id")
+	f.StringVar(&p.dstAccount, "dst-account", "", "Destination account email address")
+	f.StringVar(&p.dstCalendar, "dst-calendar", "", "Destination calendar id")
+}
 
 func (p *authListCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	if err := p.sync.Sync(ctx, "", "", "", ""); err != nil {
+	if err := p.sync.Sync(ctx, p.srcAccount, p.srcCalendar, p.dstAccount, p.dstCalendar); err != nil {
 		fmt.Println(err)
 		return subcommands.ExitFailure
 	}
